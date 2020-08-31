3f6311b362943dfe50fbd60439afa8ab2564cd5d
Kris98Wolf <kristian98farkas@gmail.com>
2020-08-31 20:13:38 +0200

    Added some components

---
+++ nomad/src/main/resources/static/components/Amenities.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="container">
+		<div id="amenities">
+			<h1>Amenity</h1>
+			<table id="tabela">
+				<tr bgcolor="lightgrey">
+					<th>ID</th>
+					<th>Name</th>
+				</tr>
+				 <tr v-for="amenity in amenities">
+					<td>{{amenity.id}}</td>
+					<td>{{amenity.name}}</td>
+					<td><button v-on:click="removeAmenity(amenity)">Delete</button>
+				</tr>
+					{{successMsg}}
+				</tr>
+			</table>
+		</div>
+	</div>
+</template>
+
+<script>
+	module.exports = {
+		data:function()
+		{
+			amenities: {},
+        	successMsg: ""
+		},
+		
+		mounted(){
+	    	var jwt = localStorage.jwt;
+	    	
+	    	if(jwt)
+			{
+		    	axios.get("rest/admin_all_amenities",{headers:{"Authorization": "Bearer " + localStorage.jwt}})
+			        .then(response => (this.amenities = response.data))
+		    		.catch(response => 
+	    			{
+	    				//TODO(Kristian): handle 404
+	    				alert("Please log in");
+	    			});
+			}
+	    },
+	    
+	    methods:
+	    {
+	        
+	    }
+	}
+</script>
---
+++ nomad/src/main/resources/static/components/PersonalData.vue
@@ -0,0 +1,102 @@
+<template>
+<div class="container">
+		<div id="personalData">
+			<h1>Personal data</h1>
+			<table id="tabela">
+				<tr>
+					<td>Username:</td>
+					<td><label v-model="guest.username"></td>
+				</tr>
+				<tr>
+					<td>Name:</td>
+					<td><input type="text" id="name" v-model="guest.name"></td>
+					<td id="emptyName">Enter your name</td>
+				</tr>
+				<tr>
+					<td>Surname:</td>
+					<td><input type="text" id="surname" v-model="guest.surname"></td>
+					<td id="emptySurname">Enter your surname</td>
+				</tr>
+				<tr>
+					<td>Sex:</td>
+					<td><select name="sex" v-model="guest.sex">
+							<option value="MALE">Male</option>
+							<option value="FEMALE">Female</option>
+							<option value="OTHER">Other</option>
+							<option value="PRIVATE">Prefer not to say</option>
+					</select></td>
+				</tr>
+				<tr>
+					<td>Password:</td>
+					<td><input type="password" id="password"
+						v-model="guest.password"></td>
+					<td id="emptyPassword">Enter your password</td>
+				</tr>
+				<tr>
+					<td>Confirm password:</td>
+					<td><input type="text" id="name" v-model="guest.name"></td>
+					<td id="emptyName">Enter your password</td>
+				</tr>
+				<tr>
+					<td><button v-on:click="updatePersonalData(guest)">Save
+							changes</button></td>
+					<td><button v-on:click="index.html">Back</button></td>
+				</tr>
+				<tr>
+					{{successMsg}}
+				</tr>
+			</table>
+		</div>
+	</div>
+</template>
+
+<script>
+	module.exports = {
+		data: function()
+        {
+            return{
+                guest: {},
+                successMsg: ""
+            }
+        },
+        mounted(){
+	    	var jwt = localStorage.jwt;
+	    	
+	    	if(jwt)
+			{
+		    	axios.get("rest/get_user",{headers:{"Authorization": "Bearer " + localStorage.jwt}})
+			        .then(response => (this.guest = response.data))
+		    		.catch(response => 
+	    			{
+	    				//TODO(Kristian): handle 404
+	    				alert("Please log in");
+	    			});
+			}
+    	},
+    	
+	    methods:
+	    {
+	        updatePersonalData: function(guest)
+	        {
+		        axios.post("rest/personal_data", guest)
+		        .then(response =>
+		            {
+		                this.successMsg = "Changes successfully saved.";
+		            })
+		        .catch(response => {
+		        		this.successMsg = "Something's wrong :(";
+		        });
+	        }
+	    }
+	}
+</script>
+
+<style scoped>
+	table, tr, td {
+		border: none;
+	}
+	
+	#emptyName, #emptySurname, #emptyPassword {
+		color: red;
+	}
+</style>
