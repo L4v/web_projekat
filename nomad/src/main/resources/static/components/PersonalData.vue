dedf1dcc6ea9a16114540b0c3ba73bab49b207da
Kris98Wolf <kristian98farkas@gmail.com>
2020-09-06 19:19:59 +0200

    Added validation to personalData

--- nomad/src/main/resources/static/components/PersonalData.vue
+++ nomad/src/main/resources/static/components/PersonalData.vue
@@ -8,12 +8,16 @@
 					<td>{{guest.username}}</td>
 				</tr>
 				<tr>
-					<td><floating-label :inputdata.sync="guest.name" placeholder="Name" type="text"></floating-label></td>
-					<td>Enter your name</td>
+					<td>
+						<small class="error">{{errors.name}}</small>
+						<floating-label :inputdata.sync="guest.name" placeholder="Name" type="text"></floating-label>
+					</td>
 				</tr>
 				<tr>
-					<td><floating-label :inputdata.sync="guest.surname" placeholder="Surname" type="text"></floating-label></td>
-					<td id="emptySurname">Enter your surname</td>
+					<td>
+						<small class="error">{{errors.surname}}</small>
+						<floating-label :inputdata.sync="guest.surname" placeholder="Surname" type="text"></floating-label>
+					</td>
 				</tr>
 				<tr>
 					<td>Sex</td>
@@ -22,15 +26,20 @@
 							<option value="FEMALE">Female</option>
 							<option value="OTHER">Other</option>
 							<option value="PRIVATE">Prefer not to say</option>
-					</select></td>
+						</select>
+					</td>
 				</tr>
 				<tr>
-					<td><floating-label :inputdata.sync="guest.password" placeholder="Password" type="password"></floating-label</td>
-					<td id="emptyPassword">Enter your password</td>
+					<td>
+						<small class="error">{{errors.password}}</small>
+						<floating-label :inputdata.sync="guest.password" placeholder="Password" type="password"></floating-label>
+					</td>
 				</tr>
 				<tr>
-					<td><floating-label :inputdata.sync="guest.password" placeholder="Confirm password" name="confirmPassword" type="password"></floating-label></td>
-					<td id="emptyConfirmPassword">Enter your password</td>
+					<td>
+						<small class="error">{{errors.confirmPassword}}</small>
+						<floating-label :inputdata.sync="guest.password" placeholder="Confirm password" name="confirmPassword" type="password"></floating-label>
+					</td>
 				</tr>
 				<tr>
 					<td><button @click="updatePersonalData(guest)">Save changes</button></td>
@@ -50,6 +59,7 @@
             return{
                 guest: {},
                 successMsg: "",
+                errors: {name: "", surname: "", password: "", confirmPassword: ""},
             }
         },
         mounted(){
@@ -69,8 +79,76 @@
     	
 	    methods:
 	    {
+			validateNameAndSurname: function()
+			{
+				if(!this.guest.name)
+                {
+                    this.errors.name = "Name must not be empty";
+                    return false;
+                }
+                if(!this.guest.surname)
+                {
+                    this.errors.surname = "Surname must not be empty";
+                    return false;
+                }
+                let regex = /^[A-Za-z]+/;
+                if(!regex.test(this.guest.name))
+                {
+                    this.errors.name = "Name must contain alphabet letters only";
+                    return false;
+                }
+                if(!regex.test(this.guest.surname))
+                {
+                    this.errors.surname = "Surname must contain alphabet letters only";
+                    return false;
+                }
+                return true;
+			},
+            validatePassword: function()
+            {
+                if(!this.guest.password)
+                {
+                    this.errors.password = "Password must not be empty";
+                    return false;
+                }
+                if(this.guest.password.length < 8)
+                {
+                    this.errors.password = "Password must be at least 8 characters long";
+                    return false;
+                }
+                let regex = /^[A-Za-z0-9!@#\$%\^&\*]+/;
+                if(!regex.test(this.guest.password))
+                {
+                    this.errors.password = "Password can contain alphabet letters, numbers and !@#$%^&* only";
+                    return false;
+                }
+                
+                if(!this.guest.confirmPassword)
+                {
+                    this.errors.confirmPassword = "Password must not be empty";
+                    return false;
+                }
+                if(this.guest.confirmPassword.equals(this.guest.password))
+                {
+                	this.errors.confirmPassword = "Not same password";
+                	return false;
+                }
+                return true;
+            },
+	    	validateFields: function()
+	    	{
+	    		this.errors.name = "";
+	        	this.errors.surname = "";
+	        	this.errors.password = "";
+	        	this.errors.confirmPassword = "";
+	        	return this.validateNameAndUsername() && this.validatePassword();
+	    	},
 	        updatePersonalData: function(guest)
 	        {        
+				/*if(!this.validateFields())
+                {
+                    return;
+                }*/
 	        
 		        axios.post("rest/personal_data", guest)
 		        .then(response =>
@@ -90,7 +168,12 @@
 		border: none;
 	}
 	
-	#emptyName, #emptySurname, #emptyPassword {
-		color: red;
-	}
+	.error
+    {
+        color: #f00;
+        padding: 0;
+        padding-bottom: 8px;
+        margin: 0;
+        font-weight: 500;
+    }
 </style>
