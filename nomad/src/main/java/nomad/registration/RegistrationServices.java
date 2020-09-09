4edc9dcfdebf77a1d8ff99ed7fc804aedca8cfdc
L4v <ivosevic.jovan@uns.ac.rs>
2020-09-09 12:51:08 +0200

    Added host registration

--- nomad/src/main/java/nomad/registration/RegistrationServices.java
+++ nomad/src/main/java/nomad/registration/RegistrationServices.java
@@ -1,14 +1,24 @@
 package nomad.registration;
 
+import static nomad.Application.key;
+import static nomad.Application.parseJws;
+import static nomad.Application.adminDAO;
 import static nomad.Application.guestDAO;
 import static nomad.Application.hostDAO;
 
+import nomad.user.UserAdmin;
 import nomad.user.UserGuest;
 import nomad.user.UserHost;
 
 import static nomad.Application.gson;
 import static nomad.utils.Responses.ok;
+import static nomad.utils.Responses.serverError;
 import static nomad.utils.Responses.badRequest;
+import static nomad.utils.Responses.forbidden;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 
 import spark.Request;
 import spark.Response;
@@ -29,10 +39,28 @@ public class RegistrationServices
 
 	public static Route registerHost = (Request request, Response response) ->
 	{
-		// TODO(Jovan): Admin validation
-		String payload = request.body();
-		UserHost host = gson.fromJson(payload, UserHost.class);
-		return registerHost(host) ? ok("Host registered", response) : badRequest("Host could not be registered", response);
+		String jws = parseJws(request);
+		if(jws == null)
+		{
+			return forbidden("User not admin", response);
+		}
+		try
+		{
+			Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jws);
+			UserAdmin admin = adminDAO.get(claims.getBody().getSubject());
+			if(admin == null)
+			{
+				return forbidden("User not admin", response);
+			}
+			
+			String payload = request.body();
+			UserHost host = gson.fromJson(payload, UserHost.class);
+			return registerHost(host) ? ok("Host registered", response) : badRequest("Host could not be registered", response);	
+		}
+		catch(Exception e)
+		{
+			return serverError("Server error: " + e.getMessage(), response);
+		}
 	};
 	
 	public static Route registerGuest = (Request request, Response response) ->
--- nomad/src/main/resources/static/components/AdminAllUsers.vue
+++ nomad/src/main/resources/static/components/AdminAllUsers.vue
@@ -15,11 +15,29 @@
             </tr>
         </table>
         <!-- TODO(Jovan): This is temporary, will add correct form later -->
-        <floating-label type="text" name="username"  placeholder="Username" :inputdata.sync="host.username"></floating-label>
-        <floating-label type="text" name="password"  placeholder="Password" :inputdata.sync="host.password"></floating-label>
-        <floating-label type="text" name="name"  placeholder="Name" :inputdata.sync="host.name"></floating-label>
-        <floating-label type="text" name="surname"  placeholder="Surname" :inputdata.sync="host.surname"></floating-label>
-        <button class="button-primary" @click="registerHost">Add new host</button>
+        <button v-if="!showForm" @click="showForm = !showForm">+</button>
+        <form v-if="showForm" @submit.prevent="registerHost" id="registration-form">
+            <b class="success">{{successMsg}}</b>
+            <small class="error">{{errors.username}}</small>
+            <floating-label type="text" name="username"  placeholder="Username" :inputdata.sync="username"></floating-label>
+            <small class="error">{{errors.password}}</small>
+            <floating-label type="password" name="password"  placeholder="Password" :inputdata.sync="password"></floating-label>
+            <small class="error">{{errors.confpassword}}</small>
+            <floating-label type="password" name="confpassword"  placeholder="Confirm password" :inputdata.sync="confpassword"></floating-label>
+            <small class="error">{{errors.name}}</small>
+            <floating-label type="text" name="name"  placeholder="Name" :inputdata.sync="name"></floating-label>
+            <small class="error">{{errors.surname}}</small>
+            <floating-label type="text" name="surname"  placeholder="Surname" :inputdata.sync="surname"></floating-label>
+            <small class="error">{{errors.sex}}</small>
+            <select name="sex" v-model="sex" required>
+                <option value="" disabled>Sex</option>
+                <option value="MALE">Male</option>
+                <option value="FEMALE">Female</option>
+                <option value="OTHER">Other</option>
+                <option value="PRIVATE">Prefer not to say</option>
+            </select>
+            <button class="button-primary" @click="registerHost">Add new host</button>
+        </form>
     </div>
 </template>
 
@@ -30,14 +48,23 @@
         {
             return {
                 users: [],
-                host: 
+                username:     "",
+                password:     "",
+                confpassword: "",
+                name:         "",
+                surname:      "",
+                sex:          "",
+                errors: 
                     {
-                        username: "",
-                        password: "",
-                        name:     "",
-                        surname:  "",
-                        sex:      "MALE",
+                        username:     "",
+                        password:     "",
+                        confpassword: "",
+                        name:         "",
+                        surname:      "",
+                        sex:          "",
                     },
+                successMsg: "",
+                showForm: false,
             }
         },
         methods:
@@ -60,12 +87,152 @@
                         // TODO(Jovan): Handle failure
                     });
             },
-            /* TODO(Jovan): Finish
+            validateName: function()
+            {
+                if(!this.name)
+                {
+                    this.errors.name = "Name can't be empty";
+                    return false;
+                }
+                let regex = /^[A-Z][a-z]*/;
+                if(!regex.test(this.name))
+                {
+                    this.errors.name = "Name must be capital and can contain alphabet letters only";
+                    return false;
+                }
+                return true;
+            },
+            validateSurname: function()
+            {
+                if(!this.surname)
+                {
+                    this.errors.surname = "Surname can't be empty";
+                    return false;
+                }
+                let regex = /^[A-Z][a-z]*/;
+                if(!regex.test(this.surname))
+                {
+                    this.errors.surname = "Surname must be capital and can contain alphabet letters only";
+                    return false;
+                }
+                return true;
+            },
+            validateSex: function()
+            {
+                if(!this.sex)
+                {
+                    this.errors.sex = "Please select an option";
+                    return false;
+                }
+                return true;
+            },
+            validateUsername: function()
+            {
+                if(!this.username)
+                {
+                    this.errors.username = "Username can't be empty";
+                    return false;
+                }
+                let regex = /^[A-Za-z]+/;
+                if(!regex.test(this.username))
+                {
+                    this.errors.username = "Username can contain alpohabet letters only";
+                    return false;
+                }
+                return true;
+            },
+            validatePassword: function()
+            {
+                if(!this.password)
+                {
+                    this.errors.password = "Password can't be empty";
+                    return false;
+                }
+                if(this.password.length < 8)
+                {
+                    this.errors.password = "Password must be at least 8 characters long";
+                    return false;
+                }
+                let regex = /^[A-Za-z0-9!@#\$%\^&\*]+/;
+                if(!regex.test(this.password))
+                {
+                    this.errors.password = "Password can contain alphabet letters, numbers and !@#$%^&* only";
+                    return false;
+                }
+                return true;
+            },
+            validateConfPassword: function()
+            {
+                if(!this.confpassword)
+                {
+                    this.errors.confpassword = "Confirmation password cannot be empty";
+                    return true;
+                }
+                if(this.confpassword.length < 8)
+                {
+                    this.errors.confpassword = "Confirmation password must be at least 8 letters long";
+                    return false;
+                }
+                let regex = /^[A-Za-z0-9!@#\$%\^&\*]+/;
+                let isValid = regex.test(this.confpassword) && (this.password === this.confpassword);
+                if(!isValid)
+                {
+                    this.errors.confpassword = "Passwords must match";
+                }
+                return isValid;
+            },
+            validateInputs: function()
+            {
+                this.errors = 
+                    {
+                        username:     "",
+                        password:     "",
+                        confpassword: "",
+                        name:         "",
+                        surname:      "",
+                        sex:          "",
+                    };
+                let nameValid = this.validateName();
+                let surnameValid = this.validateSurname();
+                let sexValid = this.validateSex();
+                let usernameValid = this.validateUsername();
+                let passwordValid = this.validatePassword();
+                let confPasswordValid = this.validateConfPassword();
+                return nameValid && surnameValid && sexValid && usernameValid
+                    && passwordValid && confPasswordValid;
+            },
+            // TODO(Jovan): Validation
             registerHost: function()
             {
-                axios.post("rest/reg_host", this.host)
+                this.successMsg = "";
+                if(!this.validateInputs())
+                {
+                    return;
+                }
+                let host = 
+                    {
+                        username:     this.username,
+                        password:     this.password,
+                        name:         this.name,
+                        surname:      this.surname,
+                        sex:          this.sex,
+                        userType:     "HOST",
+                    };
+
+                axios.post("rest/reg_host", host, {headers: {"Authorization": "Bearer " + localStorage.jwt}})
+                    .then(response => 
+                    {
+                        // TODO(Jovan): Handle success
+                        this.successMsg = "Registered";
+                        this.$router.go();
+                    })
+                    .catch(response =>
+                    {
+                        // TODO(Jovan): Handle exception
+                        this.successMsg = "Registration failed";
+                    });
             },
-            */
+            
         },
         mounted()
         {
@@ -75,5 +242,18 @@
 </script>
 
 <style>
+    .success
+    {
+        color: #f00;
+        font-weight: 500;
+        padding-bottom: 8px;
+    }
 
+    .error
+    {
+        display: block;
+        color: #f00;
+        font-weight: 500;
+        padding-bottom: 8px;
+    }
 </style>
