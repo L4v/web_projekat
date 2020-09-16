a21a4a65e845612a734cc149a3ff9dd02d96236f
L4v <ivosevic.jovan@uns.ac.rs>
2020-09-16 18:04:07 +0200

    Added logical delete for amenities

--- nomad/src/main/resources/static/components/HostComments.vue
+++ nomad/src/main/resources/static/components/HostComments.vue
@@ -4,8 +4,9 @@
         <div id="comments">
             <div v-for="comment in comments" class="comment-container">
                 <div class="comment-info">
-                    <div><b>By:</b> {{comment.comment.guestId}}</div>
-                    <div><b>Apartment:</b> {{comment.apartment.location.address.street}} {{comment.apartment.location.address.streetNumber}} {{comment.apartment.location.address.area}}</div>
+                    <div><b>By:</b>{{comment.comment.guestId}}</div>
+                    <div><b>Apartment:</b>{{comment.apartment.location.address.street}} {{comment.apartment.location.address.streetNumber}} {{comment.apartment.location.address.area}}</div>
+                    <div><b>Rating:</b>{{comment}}</div>
                 </div>
             </div>
         </div>
